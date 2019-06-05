ff7def33cb3327fba9957d8fa6f58b0cb803fb4e
Victor Velechovsky <vic.vele@gmail.com>
2019-06-05 12:16:29 -0400

    Added cmder

---
+++ fish/setup.sh
@@ -0,0 +1,5 @@
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+fisher add rafaelrinaldi/pure
+
+set LS_COLORS="di=35:ln=36:so=31:pi=31:ex=32:bd=0:cd=0:su=0:sg=0:tw=35:ow=35"
