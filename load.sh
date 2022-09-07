9e350c9306d116e3e035716ab08e885d4a39c1fb
Victor Velechosky <victorvelechosky@Victors-MacBook-Pro.local>
2022-09-06 22:32:33 -0400

    Switch to consolas. Add script to load/save

---
+++ load.sh
@@ -0,0 +1,10 @@
+cp ~/.zshrc ~/.zshrc.bak
+cp zsh/.zshrc ~/.zshrc
+
+cp ~/.gitconfig ~/.gitconfig.bak
+cp git/.gitconfig ~/.gitconfig
+
+cp ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings.json.bak
+cp vs-code/settings.json ~/Library/Application\ Support/Code/User/settings.json
+cp ~/Library/Application\ Support/Code/User/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json.bak
+cp vs-code/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
