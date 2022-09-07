034adb718e985ebe19f5ab5d344f486c7b00d1f6
velechva <vic.vele@gmail.com>
2022-09-06 22:38:13 -0400

    Add init scripts for vscode, homebrew

---
+++ homebrew/initialize.sh
@@ -0,0 +1,6 @@
+brew install zsh-autosuggestions
+brew install tree
+brew install rust
+brew install openjdk
+brew install node
+brew install neofetch
---
+++ vs-code/initialize.sh
@@ -0,0 +1,6 @@
+code --install-extension akamud.vscode-theme-onelight
+code --install-extension bungcip.better-toml
+code --install-extension ms-vscode.hexeditor
+code --install-extension rust-lang.rust-analyzer
+code --install-extension vadimcn.vscode-lldb
+code --install-extension vscodevim.vim
