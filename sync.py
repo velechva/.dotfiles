d9076e711619205315309efe74395be81b1a198d
velechva <vic.vele@gmail.com>
2022-09-10 00:53:04 -0400

    Clean up sync script

--- sync.py
+++ sync.py
@@ -4,22 +4,8 @@ import subprocess
 import sys
 import os
 
-env_home = os.getenv('HOME')
-
-def print_help_and_exit():
-    print('Syntax: ./sync.py <command>\n\nWhere <command> is one of:\n\tload\n\tsave')
-    exit(-1)
-
-def run_or_panic(cmd):
-    print('Executing: ' + ' '.join(cmd))
-    output = subprocess.run(cmd, shell=False, check=True, stdout=subprocess.PIPE)
-    if (output.stderr):
-        print(output.stderr)
-
-def fix_path(path):
-    return path.replace('~', env_home)
-
-RAW_FILES = {
+# Maps relative paths within the code repo to paths relative to $HOME
+FILES_TO_SYNC = {
     'zsh/.zshrc': '~/.zshrc',
 
     'git/.gitconfig': '~/.gitconfig',
@@ -30,27 +16,43 @@ RAW_FILES = {
     'vim/.vimrc': '~/.vimrc'
 }
 
-FILES = {}
-for file in RAW_FILES:
-    FILES[fix_path(file)] = fix_path(RAW_FILES[file])
+env_home = os.getenv('HOME')
+if env_home is None or len(env_home) < 1:
+    print("Missing $HOME environment variable")
+    exit(-1)
+
+def print_help_and_exit():
+    print('Syntax: ./sync.py <command>\n\nWhere <command> is one of:\n\tload (repo -> system)\n\tsave (system -> repo)')
+    exit(-1)
+
+def run_or_panic(cmd):
+    print('Executing: ' + ' '.join(cmd))
+    output = subprocess.run(cmd, shell=False, check=True, stdout=subprocess.PIPE)
+    if (output.stderr):
+        print(output.stderr)
+
+def process_path(path):
+    return path.replace('~', env_home)
+
+# Process each (key, value) in `FILES_TO_SYNC` and put it in `files`
+files = {}
+for file in FILES_TO_SYNC:
+    files[process_path(file)] = process_path(FILES_TO_SYNC[file])
 
 def save():
-    for repo_path in FILES:
-        system_path = FILES[repo_path]
+    for repo_path in files:
+        system_path = files[repo_path]
 
-        # save = 'cp %s %s' % (system_path, repo_path)
         save = ['cp', system_path, repo_path]
         run_or_panic(save)
 
 def load():
-    for repo_path in FILES:
-        system_path = FILES[repo_path]
+    for repo_path in files:
+        system_path = files[repo_path]
 
-        # backup = 'cp %s %s.bak' % (system_path, system_path)
         backup = ['cp', system_path, '%s.bak' % system_path]
         run_or_panic(backup)
 
-        # load   = 'cp %s %s'     % (repo_path,   system_path)
         load = ['cp', repo_path, system_path]
         run_or_panic(load)
 
