6700b59dad0a1860b0842b6f1d616a349c19e030
Victor Velechovsky <vvelecho@akamai.com>
2024-05-07 10:53:23 -0400

    Add fish shell

---
+++ fish/config.fish
@@ -0,0 +1,40 @@
+if status is-interactive
+		set un $(uname -s)
+
+		if [ "$un" = "Darwin" ]
+			defaults write com.apple.finder CreateDesktop false
+		end
+
+		export LANG=C.UTF-8
+
+		fish_vi_key_bindings --no-erase
+
+		alias ll="ls -al"
+		alias mkdir="mkdir -pv"
+		alias info="info --vi-keys"
+
+		alias g="git"
+		alias gs="git status"
+		alias greset="git reset --hard HEAD"
+
+		git config --global core.excludesfile ~/.gitignore
+
+		export EDITOR "vim"
+
+		function killall
+			ps aux | grep -i "$1" | grep -v grep | grep -v defunct | awk '{ print $2; }' | xargs kill -9
+		end
+
+		function pgrep
+				ps aux | grep -i "$1" | grep -v grep | grep -v defunct | awk '{ print $2; }'
+		end
+
+		if test -f "$HOME/.pythonrc"
+			export PYTHONSTARTUP="$HOME/.pythonrc"
+		end
+
+		if test -f ~/.config/fish/custom.fish
+			source ~/.config/fish/custom.fish
+		end
+end
+
--- symlinks.sh
+++ symlinks.sh
@@ -3,3 +3,4 @@ ln -s "$(pwd)/python/.pythonrc" ~/.pythonrc
 ln -s "$(pwd)/tmux/.tmux.conf" ~/.tmux.conf
 ln -s "$(pwd)/vim/.vimrc" ~/.vimrc
 ln -s "$(pwd)/alacritty/.alacritty.toml" ~/.alacritty.toml
+ln -s "$(pwd)/fish/config.fish" ~/.config/fish/config.fish
