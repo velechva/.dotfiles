ac1db91d88973a0e69598bbdd9f300a06199f6f9
Victor Velechovsky <vvelecho@akamai.com>
2024-07-15 08:27:47 -0400

    Set DOTFILESPATH

--- setup.sh
+++ setup.sh
@@ -1,12 +1,29 @@
 set -e
 
+# Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 git clone https://github.com/joshskidmore/zsh-fzf-history-search ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-fzf-history-search
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-./symlinks.sh
+
+read -p "Install symlinks? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    ./symlinks.sh
+fi
+
+read -p "Set DOTFILESPATH in .zshcustom?? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo "export DOTFILESPATH=$(pwd)" >> ~/.zhscustom
+fi
