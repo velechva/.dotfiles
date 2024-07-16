2fae8b8174578d1ee8f09bd10928b8f53309197d
Victor Velechovsky <vvelecho@akamai.com>
2024-07-16 17:16:25 -0400

    nvim completions + lsp

--- nvim/lua/config/completions.lua
+++ nvim/lua/config/completions.lua
@@ -13,7 +13,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ["<C-b>"]       = cmp.mapping.scroll_docs(-4),
         ["<C-f>"]       = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"]   = cmp.mapping.complete(),
+        ["<C-i>"]       = cmp.mapping.complete(),
         ["<C-e>"]       = cmp.mapping.abort(),
         ["<CR>"]        = cmp.mapping.confirm({ select = true })
     })
@@ -47,3 +47,12 @@ cmp.setup.cmdline(":", {
     matching = { disallow_symbol_nonprefix_matching = false }
 })
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lspconfig')['rust-analyzer'].setup {
+    capabilities = capabilities
+}
+
+require('lspconfig')['pyright'].setup {
+    capabilities = capabilities
+}
