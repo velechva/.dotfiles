b9ebf498070a96b12b1739f20595edf541a11683
Victor Velechovsky <vvelecho@akamai.com>
2024-09-19 13:44:29 -0400

    Tweak nvim session + quit behavior

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,3 +1,30 @@
+local function quit()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local buf_windows = vim.call("win_findbuf", bufnr)
+  local modified = vim.api.nvim_get_option_value("modified", { buf = bufnr })
+
+  if modified and #buf_windows == 1
+  then
+    vim.ui.input(
+      {
+        prompt = "... unwritten changes, want to quit? (y/n) ",
+      },
+      function(input)
+        if input == "y" then
+          vim.cmd("qa!")
+        end
+      end
+    )
+  else
+    vim.cmd("qa!")
+  end
+end
+
+vim.keymap.set("n", "<leader>q", quit, { silent = true })
+
+-- Exit VIM!
+-- vim.keymap.set("n", "<leader>qq", "<cmd>qa!<CR>", { desc = "Quit vim!" })
+
 -- File
 vim.keymap.set("n", "<leader>fs", "<cmd>write<CR>", { desc = "Save" })
 vim.keymap.set("n", "<leader>fq", "<cmd>exit<CR>", { desc = "Quit" })
