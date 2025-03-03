e01257016be57e37244503f9398da70d44ae0217
Victor Velechovsky <vvelecho@akamai.com>
2025-03-03 15:48:25 -0500

    Nvim better keybindings for cmp

--- nvim/lua/completions.lua
+++ nvim/lua/completions.lua
@@ -11,11 +11,14 @@ cmp.setup({
         documentation = cmp.config.window.bordered()
     },
     mapping = cmp.mapping.preset.insert({
-        ["<C-b>"]       = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"]       = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"]       = cmp.mapping.complete(),
-        ["<C-e>"]       = cmp.mapping.abort(),
-        ["<CR>"]        = cmp.mapping.confirm({ select = true })
+        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+
+        ["<C-u>"] = cmp.mapping.scroll_docs( 4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-e>"] = cmp.mapping.abort(),
     }),
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
